869856d07d5ecccafeee08b35d0f7e0e336db1a7
Михаил Капирусов <mihailkapirusov@MacBook-Pro-Mihail.local>
2021-03-10 13:03:10 +0300

    lr2

--- fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
+++ fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
+        System.loadLibrary("mbedcrypto");
+        initRng();
     }
 
     @Override
@@ -18,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        byte[] rnd = randomBytes(16);
+        byte[] data = {6,45,6,8,9,12,67,8};
+        byte[] encText = encrypt(rnd, data);
+        byte[] decText = decrypt(rnd, encText);
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
@@ -28,4 +34,21 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+    public static native int initRng();
+    public static native byte[] randomBytes(int no);
+    public static native byte[] encrypt(byte[] key, byte[] data);
+    public static native byte[] decrypt(byte[] key, byte[] data);
+    public static byte[] StringToHex(String s)
+    {
+        byte[] hex;
+        try
+        {
+          hex = Hex.decoderHex(s.toCharArray());
+        }
+        catch (DecoderException ex)
+        {
+            hex = null;
+        }
+        return hex;
+    }
 }
